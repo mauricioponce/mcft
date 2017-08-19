c48e05324adc6e9917f24cf86dbf13a0338375bc
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2017-08-19 14:09:14 -0300

    [ADD] bollinger bands [CHG] comments some functions for a clear look

--- code/empty_dataframe.py
+++ code/empty_dataframe.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(')(')
 
+
 def test_run(start_date, end_date):
     dates = pd.date_range(start_date, end_date, freq='30min')
 
@@ -18,11 +19,20 @@ def test_run(start_date, end_date):
 
     df = get_data(currencies, dates)
 
-    #plot_data(df)
+    # plot_data(df)
 
-    plot_selected(normalize_data(df), currencies, start_date, end_date)
+    # plot_selected(normalize_data(df), currencies, start_date, end_date)
 
-    global_statistics(df)
+    # global_statistics(df)
+
+    plot_bollinger_bands(df, 'BTC_LTC')
+
+
+def get_bollinger_bands(rm, rstd):
+    """Return upper and lower Bollinger Bands."""
+    upper_band = rm + rstd * 2
+    lower_band = rm - rstd * 2
+    return upper_band, lower_band
 
 
 def global_statistics(df):
@@ -42,6 +52,29 @@ def get_data(currencies, dates):
         df_currency = df_currency.rename(columns={'close': currency})
         df1 = df1.join(df_currency)
     return df1
+
+
+def plot_bollinger_bands(df, currency):
+    values = df[currency]
+    window = 20
+
+    rm = values.rolling(window=window, center=False).mean()
+
+    rstd = values.rolling(window=window, center=False).std()
+
+    upper_band, lower_band = get_bollinger_bands(rm, rstd)
+
+    # Plot raw SPY values, rolling mean and Bollinger Bands
+    ax = values.plot(title="Bollinger Bands", label=currency)
+    rm.plot(label='Rolling mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)
+
+    # Add axis labels and legend
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    ax.legend(loc='upper left')
+    plt.savefig('bbands.jpg')
 
 
 def plot_data(df, title='prices'):
